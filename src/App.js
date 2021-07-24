d15d7fe6e46ff72376e604479cad1ef337c6498e
Marigameo <mariappangameo@gmail.com>
2021-07-24 16:52:18 +0530

    counter app logic using internal state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+
+  const [count, setCount] = useState(0)
+  // reference - https://reactjs.org/docs/hooks-state.html
+
   return (
     <div className="App">
       <h1>My Counter app</h1>
       <div className="screen">
-        Count - 1
+        Count - {count}
       </div>
       <div className="btn-container">
-        <button className="btn btn-plus">Increment Count</button>
-        <button className="btn btn-minus">Decrement Count</button>
-        <button className="btn btn-value">Increment Count by value</button>
-        <button className="btn btn-reset">Reset Count</button>
+        <button className="btn btn-plus" onClick={() => setCount(count + 1)}>Increment Count</button>
+        <button className="btn btn-minus" onClick={() => setCount(count - 1)}>Decrement Count</button>
+        <button className="btn btn-value" onClick={() => setCount(count + 10)}>Increment Count by value</button>
+        <button className="btn btn-reset" onClick={() => setCount(0)}>Reset Count</button>
       </div>
     </div>
   );
